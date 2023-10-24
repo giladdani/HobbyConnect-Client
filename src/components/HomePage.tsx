23cfd0283042e3213051a8f103beede9f8998119
giladdanino <daninog6@gmail.com>
2023-10-24 18:11:43 +0300

    test

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -4,7 +4,7 @@ export const HomePage = () => {
 	return(
 		<div className='center_elem'>
 			<h1>Welcome to HobbyConnect!</h1>
-			<h4>The best place to find shared hobbies with complete strangers!</h4>
+			<h4>The best place to find shared hobbies with complete strangers</h4>
 		</div>
 	)
 }
